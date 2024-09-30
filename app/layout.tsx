3fcb39f32c5e0845b7ae2bfc4afb8481254ae0c6
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-09-30 22:40:17 +0800

    added aurora background

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './globals.css';
 
-import { ThemeProvider } from '@/components/theme-provider';
+import { ThemeProvider } from '@/components/providers/theme-provider';
 
 const roboto = Roboto({
   weight: ['400', '700'],
