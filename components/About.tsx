118353cc62e45875905aec4aee8886ec47988532
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-10-01 16:43:02 +0800

    implemented the about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,101 +1,87 @@
-import React from 'react';
-import { BentoGrid, BentoGridItem } from './ui/bento-grid';
-
-import {
-  TextRevealCard,
-  TextRevealCardTitle,
-  TextRevealCardDescription
-} from './ui/text-reveal-card';
+import { WobbleCard } from './ui/wobble-card';
 import { Meteors } from './ui/meteors';
-import { MagicButton } from './ui/magic-button';
 import { FlipWords } from './ui/flip-words';
-import { BackgroundGradientAnimation } from './ui/background-gradient-animation';
+import { TypewriterEffect } from './ui/typewriter-effect';
+import { BackgroundLines } from './ui/background-lines';
+import { Highlight } from './ui/hero-hightlight';
 
 export default function About() {
+  const words = [
+    {
+      text: 'A'
+    },
+    {
+      text: 'Developer',
+      className: 'text-tertiary'
+    },
+    {
+      text: 'passionate'
+    },
+    {
+      text: 'about'
+    },
+    {
+      text: 'crafting'
+    },
+    {
+      text: 'interactive'
+    },
+    {
+      text: 'and'
+    },
+    {
+      text: 'robust'
+    },
+    {
+      text: 'web'
+    },
+    {
+      text: 'applications.'
+    }
+  ];
+
+  const flipWords = ['cute', 'beautiful', 'elegant'];
+
   return (
-    <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
-      {items.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          className={item.className}
-          icon={item.icon}
-        />
-      ))}
-    </BentoGrid>
-  );
-}
-
-const flipWords = ['cute', 'beatiful', 'elegant'];
-
-const items = [
-  {
-    header: (
-      <TextRevealCard
-        text="My main focus?"
-        revealText="Developing efficient software"
-        className="h-full"
-      >
-        <TextRevealCardTitle>
-          Aiming for clean and simple code.
-        </TextRevealCardTitle>
-        <TextRevealCardDescription>
-          Hover over the card to learn more about my focus.
-        </TextRevealCardDescription>
-      </TextRevealCard>
-    ),
-    className: 'md:col-span-2'
-  },
-  {
-    header: (
-      <div className="bg-[#1d1c20] h-full rounded-lg">
-        <div className="w-full relative max-w-xs">
-          <div className="relative px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
-            <h1 className="font-bold text-xl mb-4 relative z-50">
-              Meteors because they&apos;re cool
-            </h1>
-
-            <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
-              I don&apos;t know what to write so I&apos;ll just paste something
-              cool here. One more sentence because lorem ipsum is just
-              unacceptable. Won&apos;t ChatGPT the shit out of this.
-            </p>
-
-            <Meteors number={20} />
-          </div>
+    <div className="px-5 grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
+      <WobbleCard containerClassName="col-span-1 lg:col-span-2 bg-[#1d1c20] min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+        <div className="bg-[#1d1c20] h-full w-full rounded-lg flex justify-center items-center  sm:px-5">
+          <TypewriterEffect words={words} className="text-4xl" />
         </div>
-      </div>
-    ),
-    className: 'md:col-span-1'
-  },
-  {
-    header: (
-      <BackgroundGradientAnimation className="h-full w-full">
-        <div className="absolute z-50 inset-0 flex items-center justify-center">
-          <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-            Tech enthusiast with a passion for development
+      </WobbleCard>
+      <WobbleCard containerClassName="col-span-1 bg-[#1d1c20] min-h-[300px] lg:min-h-[600px] xl:min-h-[300px]">
+        <div className="h-full w-full relative px-4 py-8 overflow-hidden rounded-2xl flex flex-col justify-center items-start">
+          <h1 className="text-3xl">
+            Fueled by <span className="text-tertiary">curiosity</span>
+          </h1>
+
+          <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
+            I combine creativity with technology to deliver efficient and
+            innovative solutions.
           </p>
+
+          <Meteors number={20} />
         </div>
-      </BackgroundGradientAnimation>
-    ),
-    className: 'md:col-span-1'
-  },
-  {
-    header: (
-      <div className="h-full w-full bg-[#1d1c20] rounded-lg flex justify-center items-center">
-        <div className="text-left space-y-4">
-          <p className="text-4xl">
+      </WobbleCard>
+      <WobbleCard containerClassName="col-span-1 bg-[#1d1c20] min-h-[300px] lg:min-h-[600px] xl:min-h-[300px]">
+        <div className="text-left space-y-4 h-full flex flex-col justify-center items-center">
+          <p className="text-3xl sm:text-4xl">
             Let&apos;s build a<FlipWords words={flipWords} />
             <br /> application together!
           </p>
-          <div className="flex justify-center">
-            <MagicButton>Copy my email address</MagicButton>
-          </div>
         </div>
-      </div>
-    ),
-    className: 'md:col-span-2'
-  }
-];
+      </WobbleCard>
+      <WobbleCard containerClassName="col-span-1 lg:col-span-2 bg-[#1d1c20] min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+        <div className="text-left space-y-4 h-full w-full flex">
+          <BackgroundLines className="flex items-center justify-center w-full h-full flex-col px-4">
+            <h2 className="text-4xl text-center">
+              Delivering{' '}
+              <Highlight>powerful, scalable, and tailored solutions</Highlight>{' '}
+              to meet the needs of businesses of all sizes.
+            </h2>
+          </BackgroundLines>
+        </div>
+      </WobbleCard>
+    </div>
+  );
+}
--- components/ui/hero-hightlight.tsx
+++ components/ui/hero-hightlight.tsx
@@ -72,7 +72,7 @@ export const Highlight = ({
       initial={{
         backgroundSize: '0% 100%'
       }}
-      animate={{
+      whileInView={{
         backgroundSize: '100% 100%'
       }}
       transition={{
@@ -86,7 +86,7 @@ export const Highlight = ({
         display: 'inline'
       }}
       className={cn(
-        `relative inline-block pb-1   px-1 rounded-lg bg-gradient-to-r from-indigo-300 to-purple-300 dark:from-indigo-500 dark:to-purple-500`,
+        `relative inline-block pb-1   px-1 rounded-lg bg-gradient-to-r from-secondary to-tertiary`,
         className
       )}
     >
