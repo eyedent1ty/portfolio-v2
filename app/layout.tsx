ebe07eb613bf8199133f732bce744c3bc3d65473
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-10-02 18:04:29 +0800

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body className={`${roboto.className} antialiased bg-secondary`}>
         <ThemeProvider
           attribute="class"
