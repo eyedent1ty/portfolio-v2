2b54feeae7df77c6016c422d56c1ecc41506a0b0
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-10-02 17:30:48 +0800

    added social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,11 @@
+import { Button } from './ui/button';
 import { MagicButton } from './ui/magic-button';
+
+import { IconBrandGithubFilled, IconBrandLinkedin } from '@tabler/icons-react';
 
 export default function Footer() {
   return (
-    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center  antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="min-h-screen relative w-full rounded-md flex md:items-center md:justify-center  antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <div className="flex flex-col items-center justify-center gap-5 p-4 mx-auto relative z-10  w-full pt-20 max-w-[1000px] md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center">
           Ready to take <span className="text-tertiary">your</span> digital
@@ -14,6 +17,27 @@ export default function Footer() {
         </p>
         <MagicButton>Let&apos;s get in touch</MagicButton>
       </div>
+      <footer className="absolute bottom-4 flex flex-col-reverse gap-2 items-center left-4 right-4 sm:flex-row sm:justify-between">
+        <p>
+          John Daniel |{' '}
+          <span className="text-tertiary">Full Stack Developer</span>
+        </p>
+        <div className="flex gap-2">
+          <Button className="p-2" variant="quaternary">
+            <a href="https://github.com/eyedent1ty" target="_blank">
+              <IconBrandGithubFilled color="#FACC15" />
+            </a>
+          </Button>
+          <Button className="p-2" variant="quaternary">
+            <a
+              href="https://www.linkedin.com/in/john-daniel-del-monte-183019236/"
+              target="_blank"
+            >
+              <IconBrandLinkedin color="#FACC15" />
+            </a>
+          </Button>
+        </div>
+      </footer>
     </div>
   );
 }
