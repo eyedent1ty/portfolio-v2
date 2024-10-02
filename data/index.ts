12d337820d0d01c9f0d68f75816ce0283b24a076
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-10-02 15:48:14 +0800

    refactor the feature section

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,14 @@
+import {
+  IconAdjustmentsBolt,
+  IconCloud,
+  IconCurrencyDollar,
+  IconEaseInOut,
+  IconHeart,
+  IconHelp,
+  IconRouteAltLeft,
+  IconTerminal2
+} from '@tabler/icons-react';
+
 export const workExperience = [
   {
     id: 1,
@@ -29,5 +40,52 @@ export const workExperience = [
     desc: 'Developed efficient payroll software using C++, streamlining payroll processes and enhancing accuracy in financial operations for the company.',
     className: 'md:col-span-2',
     thumbnail: '/exp4.svg'
+  }
+];
+
+export const features = [
+  {
+    title: 'Built for developers',
+    description:
+      'Built for engineers, developers, dreamers, thinkers and doers.',
+    icon: <IconTerminal2 />
+  },
+  {
+    title: 'Ease of use',
+    description:
+      "It's as easy as using an Apple, and as expensive as buying one.",
+    icon: <IconEaseInOut />
+  },
+  {
+    title: 'Pricing like no other',
+    description:
+      'Our prices are best in the market. No cap, no lock, no credit card required.',
+    icon: <IconCurrencyDollar />
+  },
+  {
+    title: '100% Uptime guarantee',
+    description: 'We just cannot be taken down by anyone.',
+    icon: <IconCloud />
+  },
+  {
+    title: 'Multi-tenant Architecture',
+    description: 'You can simply share passwords instead of buying new seats',
+    icon: <IconRouteAltLeft />
+  },
+  {
+    title: '24/7 Customer Support',
+    description:
+      'We are available a 100% of the time. Atleast our AI Agents are.',
+    icon: <IconHelp />
+  },
+  {
+    title: 'Money back guarantee',
+    description: 'If you donot like EveryAI, we will convince you to like us.',
+    icon: <IconAdjustmentsBolt />
+  },
+  {
+    title: 'And everything else',
+    description: 'I just ran out of copy ideas. Accept my sincere apologies',
+    icon: <IconHeart />
   }
 ];
